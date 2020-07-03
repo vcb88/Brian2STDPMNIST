953204951b063acda0fac213985dd589d12587af
Waleed El-Geresy <wbe15@ic.ac.uk>
2020-07-03 14:22:12 +0100

    data: Move Data Loading function to data

--- Diehl&Cook_spiking_MNIST_Brian2.py
+++ Diehl&Cook_spiking_MNIST_Brian2.py
@@ -10,13 +10,13 @@ import matplotlib.cm as cmap
 import time
 import os.path
 import scipy
-import pickle
 from struct import unpack
 from brian2 import *
 import os
 import brian2 as b2
-from tqdm import tqdm
 from brian2tools import *
+
+from functions.data import get_labeled_data
 
 # specify the location of the MNIST data
 MNIST_data_path = './mnist/'
@@ -24,44 +24,6 @@ MNIST_data_path = './mnist/'
 #------------------------------------------------------------------------------
 # functions
 #------------------------------------------------------------------------------
-def get_labeled_data(picklename, bTrain = True):
-    """Read input-vector (image) and target class (label, 0-9) and return
-       it as list of tuples.
-       picklename: Path to the output pickle file.
-       bTrain: True if training data, else False for test data.
-    """
-    if os.path.isfile('{}.pickle'.format(picklename)):
-        data = pickle.load(open('{}.pickle'.format(picklename), mode='rb'))
-    else:
-        # Open the images with gzip in read binary mode
-        if bTrain:
-            images = open(os.path.join(MNIST_data_path, 'train-images.idx3-ubyte'), mode='rb')
-            labels = open(os.path.join(MNIST_data_path, 'train-labels.idx1-ubyte'), mode='rb')
-        else:
-            images = open(os.path.join(MNIST_data_path, 't10k-images.idx3-ubyte'), mode='rb')
-            labels = open(os.path.join(MNIST_data_path, 't10k-labels.idx1-ubyte'), mode='rb')
-        # Get metadata for images
-        images.read(4)  # skip the magic_number
-        number_of_images = unpack('>I', images.read(4))[0]
-        rows = unpack('>I', images.read(4))[0]
-        cols = unpack('>I', images.read(4))[0]
-        # Get metadata for labels
-        labels.read(4)  # skip the magic_number
-        N = unpack('>I', labels.read(4))[0]
-
-        if number_of_images != N:
-            raise ValueError('The number of labels did not match the number of images')
-        # Get the data
-        x = np.zeros((N, rows, cols), dtype=np.uint8)  # Initialize numpy array
-        y = np.zeros((N, 1), dtype=np.uint8)  # Initialize numpy array
-        print('Unpacking {} images...'.format('training' if bTrain else 'test'))
-        for i in tqdm(range(N)):
-            x[i] = [[unpack('>B', images.read(1))[0] for unused_col in range(cols)]  for unused_row in range(rows) ]
-            y[i] = unpack('>B', labels.read(1))[0]
-
-        data = {'x': x, 'y': y, 'rows': rows, 'cols': cols}
-        pickle.dump(data, open("{}.pickle".format(picklename), "wb"))
-    return data
 
 def get_matrix_from_file(fileName):
     offset = len(ending) + 4
---
+++ functions/data.py
@@ -0,0 +1,47 @@
+'''
+A variety of functions for managing the loading of MNIST data.
+'''
+
+import os
+import pickle
+import numpy as np
+
+def get_labeled_data(picklename, bTrain = True, MNIST_data_path='./mnist'):
+    """ Read input-vector (image) and target class (label, 0-9) and return
+        it as list of tuples.
+        picklename: Path to the output pickle file.
+        bTrain: True if training data, else False for test data.
+        MNIST_data_path: Directory containing the MNIST files.
+    """
+    if os.path.isfile('{}.pickle'.format(picklename)):
+        data = pickle.load(open('{}.pickle'.format(picklename), mode='rb'))
+    else:
+        # Open the images with gzip in read binary mode
+        if bTrain:
+            images = open(os.path.join(MNIST_data_path, 'train-images.idx3-ubyte'), mode='rb')
+            labels = open(os.path.join(MNIST_data_path, 'train-labels.idx1-ubyte'), mode='rb')
+        else:
+            images = open(os.path.join(MNIST_data_path, 't10k-images.idx3-ubyte'), mode='rb')
+            labels = open(os.path.join(MNIST_data_path, 't10k-labels.idx1-ubyte'), mode='rb')
+        # Get metadata for images
+        images.read(4)  # skip the magic_number
+        number_of_images = unpack('>I', images.read(4))[0]
+        rows = unpack('>I', images.read(4))[0]
+        cols = unpack('>I', images.read(4))[0]
+        # Get metadata for labels
+        labels.read(4)  # skip the magic_number
+        N = unpack('>I', labels.read(4))[0]
+
+        if number_of_images != N:
+            raise ValueError('The number of labels did not match the number of images')
+        # Get the data
+        x = np.zeros((N, rows, cols), dtype=np.uint8)  # Initialize numpy array
+        y = np.zeros((N, 1), dtype=np.uint8)  # Initialize numpy array
+        print('Unpacking {} images...'.format('training' if bTrain else 'test'))
+        for i in tqdm(range(N)):
+            x[i] = [[unpack('>B', images.read(1))[0] for unused_col in range(cols)]  for unused_row in range(rows) ]
+            y[i] = unpack('>B', labels.read(1))[0]
+
+        data = {'x': x, 'y': y, 'rows': rows, 'cols': cols}
+        pickle.dump(data, open("{}.pickle".format(picklename), "wb"))
+    return data

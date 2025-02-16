409848ca34d8e4f89c40e012a506b5dabe7e2a1d
vcb88 <xtile@yandex.com>
2025-02-16 10:01:21 +0000

    Experiment #3: Enhance threshold adaptation: - Increase theta_plus_e from 0.06mV to 0.07mV - Build on success of previous experiment - Document hypothesis and potential risks Goal: Further reduce inactive neurons through stronger competition

--- diehl_cook_spiking_mnist_brian2.py
+++ diehl_cook_spiking_mnist_brian2.py
@@ -371,7 +371,7 @@ if test_mode:
 else:
     # Threshold adaptation parameters
     tc_theta = 1e7 * b2.ms
-    theta_plus_e = 0.06 * b2.mV  # Increased from 0.05mV for stronger adaptation
+    theta_plus_e = 0.07 * b2.mV  # Further increased from 0.06mV for stronger adaptation
     
     # Simple reset with theta modification
     scr_e = '''
